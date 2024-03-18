0b5cf932b2ec52225776361789bae5bd437ea6c4
elanfraiman <elfraiman@gmail.com>
2024-03-18 21:42:34 +0100

    feat(home): creating the bot

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import {
   IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardSubtitle,
+  IonCardTitle,
   IonContent,
   IonHeader,
+  IonImg,
   IonMenu,
   IonMenuButton,
   IonPage,
@@ -10,51 +15,65 @@ import {
   IonToolbar
 } from '@ionic/react';
 import './Home.css';
+import { useContext } from 'react';
+import { PlayerContext } from '../context/PlayerContext';
+import BotOutline from '../../resources/images/BotOutline.webp';
+
+
+const styles = {
+  notEquipped: { backgroundColor: 'rgba(214, 214, 214, 0.467)', border: '1px solid white' },
+  equipped: { backgroundColor: 'rgba(0, 255, 30, 0.23)', border: '1px solid greenyellow' }
+}
 
 const Home: React.FC = () => {
-  return (
-      <>
-        <IonMenu contentId="main-content">
-          <IonHeader>
-            <IonToolbar>
-              <IonTitle>Menu Content</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">Side menu</IonContent>
-        </IonMenu>
-        <IonPage id="main-content">
-          <IonHeader>
-            <IonToolbar>
-              <IonButtons slot="start">
-                <IonMenuButton></IonMenuButton>
-              </IonButtons>
-              <IonTitle>Home</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">
-            <IonText>
-              <div>
-                <p>Welcome to <strong>#NAME</strong></p>
-                <p>Hello there and welcome to bots4! It looks like you are new, so here are some tips:</p>
-                <ul>
-                  <li>Train against Infant and collect some kudos.</li>
-                  <li>Use the kudos in the showroom to buy weapons.</li>
-                  <li>Train against Trainmate until you gain enough experience points to reach level 2.</li>
-                  <li>Add stat points in the workshop when you level up. This is (roughly) what they do:</li>
-                  <ul>
-                    <li><strong>Strength</strong> - More damage.</li>
-                    <li><strong>Dexterity</strong> - More accuracy.</li>
-                    <li><strong>Constitution</strong> - More HP.</li>
-                    <li><strong>Intelligence</strong> - More experience/kudos.</li>
-                  </ul>
-                </ul>
-                <p>Have fun! There's no "right" way to play, so play however you want. :)</p>
-              </div>
-            </IonText>
+  const { player } = useContext(PlayerContext);
 
-          </IonContent>
-        </IonPage>
-      </>
+  return (
+    <>
+      <IonMenu contentId="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">Side menu</IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding home-bg">
+          {player ? (
+            <IonCard className="ion-padding card-fade ion-bot-card">
+              <IonCardTitle>{player.name}</IonCardTitle>
+              <IonCardSubtitle>Level: {player.level}</IonCardSubtitle>
+              <IonCardContent className="bot-card-content">
+                <IonImg src={BotOutline} className="bot-outline-img" />
+                <div className="left-arm-block" style={player.equipment?.mainHand ? styles.equipped : styles.notEquipped}>
+                </div>
+                <div className="right-arm-block" style={player.equipment?.mainHand ? styles.equipped : styles.notEquipped}>
+                </div>
+                <div className="armor-block" style={styles.notEquipped}>
+                </div>
+                <div className="helmet-block" style={styles.notEquipped}>
+                </div>
+                <div className="right-boot-block" style={styles.notEquipped}>
+
+                </div>
+                <div className="left-boot-block" style={styles.notEquipped}>
+
+                </div>
+              </IonCardContent>
+            </IonCard>
+          ) : <>Loading...</>}
+        </IonContent>
+      </IonPage>
+    </>
   );
 };
 
